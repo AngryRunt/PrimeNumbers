936546a8693835bede4f6b261500419fedfeb701
Natalia Tabunkina <nataliatabunkina@gmail.com>
2024-08-25 14:54:23 +0300

    optimized

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 
 public class Main {
     public static int divisorscounting(int start, int end) {
-        ArrayList<Integer> lst = new ArrayList<>();
+        int count = 0;
         for (int i = start; i <= end; i++) {
             int j;
             for (j = 2; j < i; j++) {
@@ -12,19 +12,20 @@ public class Main {
                 }
             }
             if (i == j) {
-                lst.add(i);
+                count++;
             }
         }
-        return lst.size();
+        return count;
     }
-    public static int eratosphen(int start, int end) {
+
+    public static ArrayList<Integer> eratosphen(int end) {
         ArrayList<Integer> lst = new ArrayList<>();
         ArrayList<Integer> tmp = new ArrayList<>();
         for (int i = 0; i <= end; i++) {
             tmp.add(i);
         }
         tmp.set(1, 0);
-       int i = start;
+       int i = 2;
         while (i <= end) {
             if (tmp.get(i) != 0) {
                 lst.add(i);
@@ -34,14 +35,24 @@ public class Main {
             }
             i++;
         }
-        return lst.size();
+        return lst;
     }
     public static void main(String[] args) {
         System.out.println("от 1 до 100 " + String.valueOf(Main.divisorscounting(2, 100)));
         System.out.println("от 100 до 1000 " + String.valueOf(Main.divisorscounting(101, 1000)));
-        int count = Main.eratosphen(2, 100);
+        int count = 0;
+        ArrayList<Integer> lst = Main.eratosphen(1000);
+        if (!lst.isEmpty()) {
+            int i = 0;
+            while ((i < lst.size()) &&
+                    (lst.get(i) < 100)) {
+                count++;
+                i++;
+            }
+
+        }
         System.out.println("Решето Эратосфена от 1 до 100 " + String.valueOf(count));
-        System.out.println("Решето Эратосфена от 100 до 1000 " + String.valueOf(Main.eratosphen(2, 1000) - count));
+        System.out.println("Решето Эратосфена от 100 до 1000 " + String.valueOf(lst.size() - count));
     }
 
 }
